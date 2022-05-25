c0260b0d19a715ef48da2c3e7fe99b18ff4a3f26
Diawara,Doudou <doudoudiawara3@gmail.com>
2022-05-24 21:24:04 -0400

    installation

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+#install plugin manager
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs\
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 #copy vimrc to ~/.vimrc and save old config to ~/.vimrc.save
 mv ~/.vimrc ~/.vimrc.save
