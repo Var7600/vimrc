d3cb9c3d2b85360141eba7f735ff0fbaff63ae20
doudou diawara <doudoudiawara3@gmail.com>
2024-04-23 15:25:27 -0400

    fix missing semicolon

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs\
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 #copy vimrc to ~/.vimrc and save old config to ~/.vimrc.save
-if [ -e ~/.vimrc ] then
+if [ -e ~/.vimrc ]; then
 	mv ~/.vimrc ~/.vimrc.save
 fi
 
